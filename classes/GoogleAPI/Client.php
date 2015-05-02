32da0ec8698ef29e670b4feb9bbd6aeef4214148
Paulo Cesar <email@pocesar.e4ward.com>
2015-05-01 23:54:21 -0300

    update config files

--- classes/GoogleAPI/Client.php
+++ classes/GoogleAPI/Client.php
@@ -5,36 +5,37 @@ class GoogleAPI_Client extends Google_Client {
 	 * @var Config_Group
 	 */
 	public $config;
-	
+
 	public static $token_name = 'googleapi_token';
-	
+
 	public function __construct()
 	{
 		$this->config = Kohana::$config->load('googleapi');
-		
-		if (empty($this->config->oauth2_client_id))
-		{
-			throw new Kohana_Kohana_Exception('You must fill the oauth2_client_id in the config file');
-		} 
-		elseif (empty($this->config->oauth2_client_secret))
-		{
-			throw new Kohana_Kohana_Exception('You must fill the oauth2_client_secret in the config file');
-		} 
-		elseif (empty($this->config->oauth2_redirect_uri))
-		{
-			throw new Kohana_Kohana_Exception('You must fill the oauth2_redirect_url in the config file');
-		} 
-		elseif (empty($this->config->developer_key))
+
+		if (empty($this->config->developer_key))
 		{
 			throw new Kohana_Kohana_Exception('You must fill the developer_key in the config file');
 		}
-		
+		if (empty($this->config->oauth2_client_id))
+		{
+			throw new Kohana_Kohana_Exception('You must fill the oauth2_client_id in the config file');
+		}
+		elseif (empty($this->config->oauth2_client_secret))
+		{
+			throw new Kohana_Kohana_Exception('You must fill the oauth2_client_secret in the config file');
+		}
+		elseif (empty($this->config->oauth2_redirect_uri))
+		{
+			throw new Kohana_Kohana_Exception('You must fill the oauth2_redirect_url in the config file');
+		}
+
+
 		@session_start();
-		
-		parent::__construct($this->config->as_array()); 
-		
+
+		parent::__construct($this->config->as_array());
+
 		$this->setApprovalPrompt("auto");
-		
+
 		$this->setScopes(
 			array_merge((array)$this->config->services['oauth2']['scope'], array($this->config->services['plus']['scope']))
 		);
@@ -47,7 +48,7 @@ class GoogleAPI_Client extends Google_Client {
 			$this->authenticate($code);
 			$_SESSION[GClient::$token_name] = $this->getAccessToken();
 		}
-		else 
+		else
 		{
 			$this->setAccessToken($_SESSION[GClient::$token_name]);
 		}
--- config/googleapi.php
+++ config/googleapi.php
@@ -1,65 +1,101 @@
 <?php
 
 return array(
-	// True if objects should be returned by the service classes.
-	// False if associative arrays should be returned (default behavior).
-	'use_objects'           => true,
+  'application_name' => '',
 
-	// The application_name is included in the User-Agent HTTP header.
-	'application_name'      => '',
+  // Which Authentication, Storage and HTTP IO classes to use.
+  //'auth_class'    => 'Google_Auth_OAuth2',
+  //'cache_class'   => 'Google_Cache_File',
+  //'logger_class'  => 'Google_Logger_Null',
 
-	// OAuth2 Settings, you can get these keys at https://code.google.com/apis/console
-	'oauth2_client_id'      => '',
-	'oauth2_client_secret'  => '',
-	'oauth2_redirect_uri'   => URL::site(Route::get('googleapi-auth')->uri(), Request::$current),
+  // Don't change these unless you're working against a special development
+  // or testing environment.
+  //'base_path' => 'https://www.googleapis.com',
 
-    // If you don't use a pop-up window, you can specify a different path to go to after auth has finished
-    'post_auth_url'         => '',
+  // Definition of class specific values, like file paths and so on.
+  'classes' => array(
+    'Google_IO_Abstract' => array(
+      'request_timeout_seconds' => 100,
+    ),
+    /*'Google_Logger_Abstract' => array(
+      'level' => 'debug',
+      'log_format' => "[%datetime%] %level%: %message% %context%\n",
+      'date_format' => 'd/M/Y:H:i:s O',
+      'allow_newlines' => true
+    ),
+    'Google_Logger_File' => array(
+      'file' => 'php://stdout',
+      'mode' => 0640,
+      'lock' => false,
+    ),*/
+    /*'Google_Http_Request' => array(
+      // Disable the use of gzip on calls if set to true. Defaults to false.
+      'disable_gzip' => Google_Config::GZIP_ENABLED,
 
-	// The developer key, you get this at https://code.google.com/apis/console
-	'developer_key'         => '',
+      // We default gzip to disabled on uploads even if gzip is otherwise
+      // enabled, due to some issues seen with small packet sizes for uploads.
+      // Please test with this option before enabling gzip for uploads in
+      // a production environment.
+      'enable_gzip_for_uploads' => Google_Config::GZIP_UPLOADS_DISABLED,
+    ),*/
+    // If you want to pass in OAuth 2.0 settings, they will need to be
+    // structured like this.
+    'Google_Auth_OAuth2' => array(
+      // Keys for OAuth 2.0 access, see the API console at
+      // https://developers.google.com/console
+      'client_id' => '',
+      'client_secret' => '',
+      'redirect_uri' => '',
 
-	// Site name to show in the Google's OAuth 1 authentication screen.
-	'site_name'             => 'www.example.org',
+      // Simple API access key, also from the API console. Ensure you get
+      // a Server key, and not a Browser key.
+      'developer_key' => '',
 
-	// Which Authentication, Storage and HTTP IO classes to use.
-	'authClass'             => 'Google_OAuth2',
-	'ioClass'               => 'Google_CurlIO',
-	'cacheClass'            => 'Google_FileCache',
-
-	// Don't change these unless you're working against a special development or testing environment.
-	'basePath'              => 'https://www.googleapis.com',
-
-	// IO Class dependent configuration, you only have to configure the values
-	// for the class that was configured as the ioClass above
-	'ioFileCache_directory' => APPPATH . 'cache/Google_Client',
-
-	// Definition of service specific values like scopes, oauth token URLs, etc
-	'services'              => array(
-		'analytics'        => array('scope' => 'https://www.googleapis.com/auth/analytics.readonly'),
-		'calendar'         => array(
-			'scope' => array(
-				"https://www.googleapis.com/auth/calendar",
-				"https://www.googleapis.com/auth/calendar.readonly",
-			)
-		),
-		'books'            => array('scope' => 'https://www.googleapis.com/auth/books'),
-		'latitude'         => array(
-			'scope' => array(
-				'https://www.googleapis.com/auth/latitude.all.best',
-				'https://www.googleapis.com/auth/latitude.all.city',
-			)
-		),
-		'moderator'        => array('scope' => 'https://www.googleapis.com/auth/moderator'),
-		'oauth2'           => array(
-			'scope' => array(
-				'https://www.googleapis.com/auth/userinfo.profile',
-				'https://www.googleapis.com/auth/userinfo.email',
-			)
-		),
-		'plus'             => array('scope' => 'https://www.googleapis.com/auth/plus.login'),
-		'siteVerification' => array('scope' => 'https://www.googleapis.com/auth/siteverification'),
-		'tasks'            => array('scope' => 'https://www.googleapis.com/auth/tasks'),
-		'urlshortener'     => array('scope' => 'https://www.googleapis.com/auth/urlshortener')
-	)
+      // Other parameters.
+      'hd' => '',
+      'prompt' => '',
+      'openid.realm' => '',
+      'include_granted_scopes' => '',
+      'login_hint' => '',
+      'request_visible_actions' => '',
+      'access_type' => 'online',
+      'approval_prompt' => 'auto',
+      'federated_signon_certs_url' =>
+          'https://www.googleapis.com/oauth2/v1/certs',
+    ),/*
+    'Google_Task_Runner' => array(
+      // Delays are specified in seconds
+      'initial_delay' => 1,
+      'max_delay' => 60,
+      // Base number for exponential backoff
+      'factor' => 2,
+      // A random number between -jitter and jitter will be added to the
+      // factor on each iteration to allow for better distribution of
+      // retries.
+      'jitter' => .5,
+      // Maximum number of retries allowed
+      'retries' => 0
+    ),
+    'Google_Service_Exception' => array(
+      'retry_map' => array(
+        '500' => Google_Config::TASK_RETRY_ALWAYS,
+        '503' => Google_Config::TASK_RETRY_ALWAYS,
+        'rateLimitExceeded' => Google_Config::TASK_RETRY_ALWAYS,
+        'userRateLimitExceeded' => Google_Config::TASK_RETRY_ALWAYS
+      )
+    ),
+    'Google_IO_Exception' => array(
+      'retry_map' => !extension_loaded('curl') ? array() : array(
+        CURLE_COULDNT_RESOLVE_HOST => Google_Config::TASK_RETRY_ALWAYS,
+        CURLE_COULDNT_CONNECT => Google_Config::TASK_RETRY_ALWAYS,
+        CURLE_OPERATION_TIMEOUTED => Google_Config::TASK_RETRY_ALWAYS,
+        CURLE_SSL_CONNECT_ERROR => Google_Config::TASK_RETRY_ALWAYS,
+        CURLE_GOT_NOTHING => Google_Config::TASK_RETRY_ALWAYS
+      )
+    ),
+    // Set a default directory for the file cache.
+    /*'Google_Cache_File' => array(
+      'directory' => sys_get_temp_dir() . '/Google_Client'
+    )*/
+  )
 );
