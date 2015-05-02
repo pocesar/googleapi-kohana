cf0b715449d0211ee497e3f47180dffdc0d6f334
Paulo Cesar <email@pocesar.e4ward.com>
2015-05-01 23:54:58 -0300

    update config files

--- classes/GoogleAPI/Client.php
+++ classes/GoogleAPI/Client.php
@@ -12,33 +12,9 @@ class GoogleAPI_Client extends Google_Client {
 	{
 		$this->config = Kohana::$config->load('googleapi');
 
-		if (empty($this->config->developer_key))
-		{
-			throw new Kohana_Kohana_Exception('You must fill the developer_key in the config file');
-		}
-		if (empty($this->config->oauth2_client_id))
-		{
-			throw new Kohana_Kohana_Exception('You must fill the oauth2_client_id in the config file');
-		}
-		elseif (empty($this->config->oauth2_client_secret))
-		{
-			throw new Kohana_Kohana_Exception('You must fill the oauth2_client_secret in the config file');
-		}
-		elseif (empty($this->config->oauth2_redirect_uri))
-		{
-			throw new Kohana_Kohana_Exception('You must fill the oauth2_redirect_url in the config file');
-		}
-
-
 		@session_start();
 
 		parent::__construct($this->config->as_array());
-
-		$this->setApprovalPrompt("auto");
-
-		$this->setScopes(
-			array_merge((array)$this->config->services['oauth2']['scope'], array($this->config->services['plus']['scope']))
-		);
 	}
 
 	function authenticateCode($code = null)
